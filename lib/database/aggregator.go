29da3aa6d6eff25196e50a2dc0d85759008f4c5b
Leonardo Ferreira <leonardoff.eng@gmail.com>
2024-01-07 14:37:39 +0100

    add AvgRewardWhenElligible to aggregator

--- lib/database/aggregator.go
+++ lib/database/aggregator.go
@@ -9,9 +9,10 @@ type DatabaseAggregator struct {
 }
 
 type ArmStats struct {
-	count             int
-	AvgRewardWhenUsed float64
-	// avgRewardWhenElligible float64
+	count                  int
+	AvgRewardWhenUsed      float64
+	AvgRewardWhenElligible float64
+	sumScoresWhenElligible float64
 }
 
 type ArmToStats map[*bandit.Arm]*ArmStats
@@ -34,25 +35,25 @@ func (db *DatabaseAggregator) ArmToStats() ArmToStats {
 	armToStats := make(ArmToStats)
 	for _, entry := range db.Entries {
 		curr := armToStats[entry.ChosenArm]
-
 		if curr == nil {
 			curr = &ArmStats{}
 			armToStats[entry.ChosenArm] = curr
 		}
-
 		curr.count++
 		curr.AvgRewardWhenUsed = curr.AvgRewardWhenUsed + (entry.Reward-curr.AvgRewardWhenUsed)/float64(curr.count)
 
-		// for _, arm := range entry.EligibleArms {
-		// 	curr := armToStats[arm]
+		for arm, score := range *entry.ArmToScore {
+			curr = armToStats[arm]
+			if curr == nil {
+				curr = &ArmStats{}
+				armToStats[arm] = curr
+			}
 
-		// 	if curr == nil {
-		// 		curr = &ArmStats{}
-		// 		armToStats[arm] = curr
-		// 	}
-
-		// 	curr.avgRewardWhenElligible = curr.avgRewardWhenElligible + (entry.Reward-curr.avgRewardWhenElligible)/float64(curr.count)
-		// }
+			// sum (score * reward) / sum (score)
+			dividend := curr.sumScoresWhenElligible + score
+			curr.AvgRewardWhenElligible = (curr.AvgRewardWhenElligible * curr.sumScoresWhenElligible / dividend) + (score * entry.Reward / dividend)
+			curr.sumScoresWhenElligible += score
+		}
 	}
 	return armToStats
 }
